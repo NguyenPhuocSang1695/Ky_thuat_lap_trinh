f2ea9b9732ef3da4c9e8d497ee788fa6d969295d
NguyenPhuocSang1695 <158601013+NguyenPhuocSang1695@users.noreply.github.com>
2024-04-27 22:16:30 +0700

    ex-1.6.cpp

--- ex-1.6.cpp
+++ ex-1.6.cpp
@@ -1,20 +1,39 @@
 #include<iostream>
 using namespace std;
 
-int a(int n, int i, int sum= 0){
-	if (i== n){
-		return sum;
+void tinh(int a, int mang[], int i = 0, int x = 0) {
+    if (i < a) {
+        x += mang[i] * (a - i); // cong thuc tong quat
+        tinh(a, mang, i + 1, x);
+    } else {
+        mang[a] = x;
+    }
+}
+
+void ham(int a, int mang[]) {
+	int i= 0, x= 0;
+	if(a>2) // a= 0 va a= 1 thi ham bang 1
+	{
+	a=a-1;
+	ham(a, mang);
+	tinh(a, mang, i, x);
 	}
-	sum+= (n-i)*a(n+1, i, sum);
-	return a(n, i+1, sum);
 }
 
-int b(int n){
-	return a(n, 0, 0);
-}
 
-int main(){
-    int n; cin >> n;
-    cout << b(n);
-    return 0;
+int main() {
+	int mang1[8];
+	mang1[0]=1;
+	mang1[1]=1;
+	int n;
+	ham(8, mang1);
+	cout << "So x7 la: " << mang1[7] <<endl;
+	int n; cin >> n;
+	int mang2[n]; 
+	mang2[0]=1; // dieu kien ban dau cua ham
+	mang2[1]=1; // dieu kien ban dau cua ham
+	ham(n+1, mang2);
+	cout << "Gia tri so x" << n << " la: ";
+	cout << mang2[n];
+	return 0;
 }
