1a6f471fc84d25eb3f024a1bb7b378acba56c71f
NguyenPhuocSang1695 <158601013+NguyenPhuocSang1695@users.noreply.github.com>
2024-04-28 16:35:22 +0700

    Update ex-1.6.cpp

--- ex-1.6.cpp
+++ ex-1.6.cpp
@@ -1,40 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int *tinh (int a, int mang[], int i= 0, int x = 0){
-	if (i== a){
-		mang[a]=x;
-		return mang;	
-	} 
-	x= x+mang[i]*(a-i);
-	return tinh (a, mang, i+1, x);
+int calculate(int n, int i= 0, int sum= 0){
+    if(n == 1 || n == 0)
+        return 1;
+    else{
+        if (i== n){
+        	return sum;
+		}
+        sum += (n - i) * calculate(i);
+        return calculate (n, i+1, sum);
+    }
+    return 0;
 }
 
-void ham(int a, int mang[]) {
-	int i= 0, x= 0;
-	if(a>2) // a= 0 va a= 1 thi ham bang 1
-	{
-	a=a-1;
-	ham(a, mang);
-	tinh(a, mang, i, x);
-	}
-}
-
-
-int main() {
-	int mang1[8];
-	mang1[0]=1;
-	mang1[1]=1;
-	int n;
-	ham(8, mang1);
-	cout << "So x7 la: " << mang1[7] <<endl;
-	cout << "n= ";
-	cin >> n;
-	int mang2[n]; 
-	mang2[0]=1; // dieu kien ban dau cua ham
-	mang2[1]=1; // dieu kien ban dau cua ham
-	ham(n+1, mang2);
-	cout << "Gia tri so x" << n << " la: ";
-	cout << mang2[n];
-	return 0;
+int main(){
+    int n;
+    cin >> n;
+    cout << "x" << n << " = " << calculate(n) << endl;
 }
